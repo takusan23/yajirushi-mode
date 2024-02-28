6d3cd2760c7f0bb47cb8d1da0c1e788123602d18
takusan23 <takumu1200@gmail.com>
2024-02-29 03:44:58 +0900

    React で ui.html 相当の画面を実装

---
+++ src-ui/App.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <h2>Rectangle Creator</h2>
+            <p>
+                Count:
+                <input
+                    onChange={(ev) => setCount(Number(ev.target.value))}
+                    value={count} />
+            </p>
+            <button
+                onClick={() => {
+                    parent.postMessage({ pluginMessage: { type: 'create-rectangles', count } }, '*')
+                }}
+            >
+                Create
+            </button>
+
+            <button
+                onClick={() => {
+                    parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*')
+                }}
+            >
+                Cancel
+            </button>
+        </>
+    )
+}
+
+export default App
