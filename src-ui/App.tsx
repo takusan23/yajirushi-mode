91c976dac7260e23332c2309c7555cf60dae84ff
takusan23 <takumu1200@gmail.com>
2024-02-29 04:35:08 +0900

    Tailwind CSS を入れて見た目を調整した

--- src-ui/App.tsx
+++ src-ui/App.tsx
@@ -1,18 +1,21 @@
 import { useState } from 'react'
+import "./App.css"
 
 function App() {
     const [count, setCount] = useState(5)
 
     return (
-        <>
-            <h2>Rectangle Creator</h2>
+        <div className='flex flex-col space-y-2 items-center'>
+            <h2 className='text-3xl'>Rectangle Creator</h2>
             <p>
                 Count:
                 <input
+                    className='border-black border-b-2 ml-2'
                     onChange={(ev) => setCount(Number(ev.target.value))}
                     value={count} />
             </p>
             <button
+                className='rounded-md border-blue-300 border-2 px-4'
                 onClick={() => {
                     parent.postMessage({ pluginMessage: { type: 'create-rectangles', count } }, '*')
                 }}
@@ -21,13 +24,14 @@ function App() {
             </button>
 
             <button
+                className='rounded-md border-red-300 border-2 px-4'
                 onClick={() => {
                     parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*')
                 }}
             >
                 Cancel
             </button>
-        </>
+        </div>
     )
 }
 
