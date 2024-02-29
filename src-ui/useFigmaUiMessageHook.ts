3f61ee18a15aad584526a64c83ece99057b9857e
takusan23 <takumu1200@gmail.com>
2024-03-01 01:07:31 +0900

    react hook

---
+++ src-ui/useFigmaUiMessageHook.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+import FigmaUiMessageTool from "./FigmaUiMessageTool"
+import { MessageTypes } from "../src-common/MessageTypes"
+
+/** {@link FigmaUiMessageTool}を React で使えるようにしたカスタムフック */
+function useFigmaUiMessageHook() {
+
+    // メッセージを受け取る
+    const [message, setMessage] = useState<MessageTypes>()
+    useEffect(() => {
+        FigmaUiMessageTool.receiveMessage((receiveMessage) => {
+            setMessage(receiveMessage)
+        })
+    }, [])
+
+    /** {@link MessageTypes}を Figma プラグインへ送る */
+    function postMessage(message: MessageTypes) {
+        FigmaUiMessageTool.postMessage(message)
+    }
+
+    return {
+        message,
+        postMessage
+    }
+}
+
+export default useFigmaUiMessageHook
